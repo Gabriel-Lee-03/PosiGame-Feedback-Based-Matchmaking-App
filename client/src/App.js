bc928f775d07546c4f2f5be7792157ac918764a6
lst20 <lst20@ic.ac.uk>
2023-05-31 15:24:39 +0100

    testing updates can be deployed by pipeline

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
+        <form>
+          <label for="fname">First name:</label><br></br>
+          <input type="text" id="fname" name="fname" value="John"></input>
+          <label for="lname">Last name:</label>
+          <input type="text" id="lname" name="lname" value="Doe"></input>
+          <input type="submit" value="Submit"></input>
+        </form> 
       </header>
     </div>
   );
