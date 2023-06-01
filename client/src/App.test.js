5b3c5e2358f46342c091657854827f239ad933e3
Tsz Chun Jason Wong <tcw121@cloud-vm-45-94>
2023-06-01 15:59:18 +0100

    Added front end test and test script in pipeline

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Clicking Add Player changes to Screen 1', () => {
+  const { getByText } = render(<App />);
+  
+  // Find the "Add Player" button in Screen 2 and click it
+  const addButton = getByText('Add Player');
+  fireEvent.click(addButton);
+  
+  // Check if Screen 1 is rendered after clicking the button
+  const screen1 = getByText('Player');
+  expect(screen1).toBeInTheDocument();
 });
