0b4fd8f5871c5fdf5c26fae3c8c98f196ba0282d
lst20 <lst20@ic.ac.uk>
2023-06-02 01:40:02 +0100

    remove comment

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,7 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 
-const apiUrl = "/api";
-
-// function componentDidMount(setState) {
-//   try {
-//     axios.get(apiUrl)
-//     .then(res => {
-//       const data = res.data;
-//       setState(data);
-//     });
-//   }catch(error) {
-//     console.log(error);
-//   }
-// }
-
-// async function handleSubmit(e, setText, setData) {
-//   console.log(`ok`);
-//   e.preventDefault();
-//   try {
-//     console.log("value to post: " + e.target.name.value);
-//     const response = await axios.post(apiUrl, {player: e.target.name.value});
-//     setText('');
-//   } catch (error) {
-//     // Request was not successful
-//     console.error('An error occurred:', error);
-//   }
-// }
-
-// function Appis() {
-//   const defaultNames = [{name: `Ann Frank`, id: 0}, {name: `Frank Sinatra`, id: 1}];
-//   const [data, setData] = useState(defaultNames);
-//   const [input, setCurrentName] = useState(``);
-
-//   componentDidMount(setData);
-
-//   return (
-//     <div className="App">
-//       <img src={logo} className="App-logo" alt="logo" />
-//       <p>{!data ? `Loading...` : `data loaded`}</p>
-//       <form onSubmit={(e) => {handleSubmit(e, setCurrentName, setData)}}>
-//         <label for="name">First name: </label>
-//         <input 
-//           onChange={(e) => setCurrentName(e.target.value)}
-//           type="text" id="name" name="name" value={input}>
-//         </input><br></br>
-//         <button type="submit">Submit</button>
-//       </form>
-//       <div>
-//         <p>Users of our website include:</p>
-//         {
-//           data.map((name) =>
-//             {return(<p key={name.id}>{name.name}</p>)})
-//         }
-//       </div>
-//     </div>
-//   );
-// }
-//////////////////////////////////////////////////
 // Screen 1 component
 function Screen1({ onSubmit }) {
   // State for Game ID and Name inputs
