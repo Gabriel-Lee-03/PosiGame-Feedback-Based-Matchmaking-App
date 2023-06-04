03fbbb15daf7e9367112de79d39084dce92c99ae
lst20 <lst20@ic.ac.uk>
2023-06-05 00:31:53 +0100

    hide pw to db in .env

--- server/db.js
+++ server/db.js
@@ -1,34 +1,17 @@
 const mongoose = require("mongoose");
 
-// const MONGO_API_KEY = process.env.MONGO_KEY;
+const MONGO_API_KEY = process.env.MONGO_KEY;
 const dbName = "players";
 
-// const TestPlayer = require("./models/player");
-
-const uri = "mongodb+srv://drp22-admin:"+"Drp2213579"+
+const uri = "mongodb+srv://drp22-admin:"+MONGO_API_KEY+
             "@cluster0.nyxdbux.mongodb.net/" + dbName +
             "?retryWrites=true&w=majority";
-
-// const uri = "mongodb://drp22-admin:"+"Drp2213579@"+
-//             "ac-7jak6sg-shard-00-00.nyxdbux.mongodb.net:27017,"+
-//             "ac-7jak6sg-shard-00-01.nyxdbux.mongodb.net:27017,"+
-//             "ac-7jak6sg-shard-00-02.nyxdbux.mongodb.net:27017/"+ 
-//             "?retryWrites=true&w=majority";
-
-
-
-
-
-// // Import the mongoose module
-
 
 // // Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
 // // Included because it removes preparatory warnings for Mongoose 7.
 // // See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
 // mongoose.set("strictQuery", false);
 // // Define the database URL to connect to.
-// const mongoDB = uri;
-
 // // Wait for database to connect, logging an error if there is a problem
 
 module.exports = async () => {
@@ -41,18 +24,3 @@ module.exports = async () => {
   }
 }
 
-// const {MongoClient} = require('mongodb');
-
-// module.exports = async () => {
-//   const client = new MongoClient(uri);
-//   try {
-//     const database = client.db('sample_mflix');
-//     const movies = database.collection('movies');
-//     // Query for a movie that has the title 'Back to the Future'
-//     const query = { title: 'Back to the Future' };
-//     const movie = await movies.findOne(query);
-//     console.log(movie);
-//   } catch (error) {
-//     console.log("Could not connect to database.", error);
-//   }
-// }
