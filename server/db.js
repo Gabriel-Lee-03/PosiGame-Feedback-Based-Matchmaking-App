8fbbf2ac9187f473c921b951fa619fe4ec2d059c
lst20 <lst20@ic.ac.uk>
2023-06-05 00:47:47 +0100

    fix .eng impl

--- server/db.js
+++ server/db.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+const util = require('util');
 
+dotenv.config();
 const MONGO_API_KEY = process.env.MONGO_KEY;
-const dbName = "players";
+const USER = process.env.USER;
 
-const uri = "mongodb+srv://drp22-admin:"+MONGO_API_KEY+
+const dbName = "players";
+const uri = "mongodb+srv://"+USER+":"+MONGO_API_KEY+
             "@cluster0.nyxdbux.mongodb.net/" + dbName +
             "?retryWrites=true&w=majority";
 
@@ -16,7 +20,6 @@ const uri = "mongodb+srv://drp22-admin:"+MONGO_API_KEY+
 
 module.exports = async () => {
   try{
-    console.log("func is called");
     await mongoose.connect(uri);
     console.log("Connected to database.");
   } catch (error) {
@@ -24,3 +27,18 @@ module.exports = async () => {
   }
 }
 
+// const {MongoClient} = require('mongodb');
+
+// module.exports = async () => {
+//   const client = new MongoClient(uri);
+//   try {
+//     const database = client.db('sample_mflix');
+//     const movies = database.collection('movies');
+//     // Query for a movie that has the title 'Back to the Future'
+//     const query = { title: 'Back to the Future' };
+//     const movie = await movies.findOne(query);
+//     console.log(movie);
+//   } catch (error) {
+//     console.log("Could not connect to database.", error);
+//   }
+// }
