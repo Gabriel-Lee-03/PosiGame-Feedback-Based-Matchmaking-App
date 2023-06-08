64de269767db5f6b5e59380d8ef5a316e0d4ed57
Jason Wong <tcjasonwong@gmail.com>
2023-06-08 18:33:08 +0100

    Added pop up explanation of rating in lobby

--- client/src/App.js
+++ client/src/App.js
@@ -168,7 +168,16 @@ function Lobby({ onAddPlayer, nameVal }) {
             <th>Username</th>
             <th>Game ID</th>
             <th>Friendliness</th>
-            <th>Rating</th>
+            <th>Rating {
+              <div className="question__container">
+              <button className="question__button">?</button>
+              <div className="question__popup">Ever been flamed in game? Teammates intentionally feeding and throwing games? Seen or heard discriminatory comments that made you or others uncomfortable? Most gamers have experienced some level of toxicity when playing online games. These ratings allow us to matchmake based on your friendliness, and to promote a healthier and more positive gaming environment. 
+
+After you are matched with a team and have played together, you can help by rating your teammates based on how friendly or toxic they were. We will collect this information to calculate a friendliness rating for each player. 
+
+When searching for players, you will be matched with others who have a similar rating as the average rating amongst the players currently in your lobby. This means that the better you behave and the more positive you are, the more likely you will be matched with friendlier players. On the other hand, if your teammates feel that you are being rude, toxic, or otherwise detrimental to the gaming environment and your fellow gamers’ experience, you will be matched with others like that until you improve your behaviour. </div>
+              </div> }
+            </th>
           </tr>
         </thead>
         <tbody>
